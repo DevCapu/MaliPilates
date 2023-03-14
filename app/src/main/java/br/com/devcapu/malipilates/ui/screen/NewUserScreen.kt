9446d24ce18cc1dbb18375c8b94d742c2e249a45
Felipe Moreno Borges <felipe.b2014@gmail.com>
2023-03-14 14:56:21 -0300

    Add font and refactor selection box

--- app/src/main/java/br/com/devcapu/malipilates/ui/screen/NewUserScreen.kt
+++ app/src/main/java/br/com/devcapu/malipilates/ui/screen/NewUserScreen.kt
@@ -1,33 +1,46 @@
 package br.com.devcapu.malipilates.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.selection.selectableGroup
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.devcapu.malipilates.ui.theme.MaliPilatesTheme
+import br.com.devcapu.malipilates.ui.theme.robotoSerif
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NewUserScreen(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
     LazyColumn(
         modifier = modifier.fillMaxWidth(),
         horizontalAlignment = CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
+        item {
+            Text(
+                text = "Dados pessoais",
+                fontFamily = robotoSerif,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color(0xFF3F3F3F)
+            )
+        }
         item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
@@ -36,7 +49,9 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "João Gomes") },
                 onValueChange = { }
             )
+        }
 
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
@@ -44,7 +59,9 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "@user") },
                 onValueChange = { }
             )
+        }
 
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
@@ -52,7 +69,8 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "(99) 99999-9999") },
                 onValueChange = { }
             )
-
+        }
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
@@ -60,7 +78,9 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "(99) 99999-9999") },
                 onValueChange = { }
             )
+        }
 
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
@@ -68,7 +88,9 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "(99) 99999-9999") },
                 onValueChange = { }
             )
+        }
 
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
@@ -76,7 +98,8 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "(99) 99999-9999") },
                 onValueChange = { }
             )
-
+        }
+        item {
             OutlinedTextField(
                 modifier = Modifier.fillMaxWidth(),
                 value = "",
@@ -84,46 +107,55 @@ fun NewUserScreen(modifier: Modifier = Modifier) {
                 placeholder = { Text(text = "(99) 99999-9999") },
                 onValueChange = { }
             )
+        }
 
-            Column(Modifier.selectableGroup().fillMaxWidth()) {
-                Text(text = "Como nos encontrou")
+        item {
+            Surface(
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .fillMaxWidth()
+                    .pointerInput(Unit) {
+                        detectTapGestures(onTap = { expanded = !expanded })
+                    },
+                shape = RoundedCornerShape(4.dp),
+                border = BorderStroke(1.dp, Black)
+            ) {
                 Row(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier.padding(16.dp),
                     verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    RadioButton(
-                        selected = true,
-                        onClick = {  },
-                        modifier = Modifier.semantics { contentDescription = "Localized Description" }
+                    Text(text = "Como nos encontrou?")
+                    Icon(
+                        imageVector = Icons.Default.ArrowDropDown,
+                        contentDescription = "Ver opções de como nos encontrou"
                     )
-                    Text(text = "Indicação")
                 }
+            }
 
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    RadioButton(
-                        selected = true,
-                        onClick = {  },
-                        modifier = Modifier.semantics { contentDescription = "Localized Description" }
-                    )
-                    Text(text = "Localização")
-                }
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp)
-                ) {
-                    RadioButton(
-                        selected = true,
-                        onClick = {  },
-                        modifier = Modifier.semantics { contentDescription = "Localized Description" }
-                    )
-                    Text(text = "Internet")
-                }
+            DropdownMenu(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Indicação") },
+                    onClick = { /* Handle edit! */ },
+                )
+                DropdownMenuItem(
+                    text = { Text("Localização") },
+                    onClick = { /* Handle settings! */ },
+                )
+                DropdownMenuItem(
+                    text = { Text("Internet") },
+                    onClick = { /* Handle send feedback! */ },
+                )
+                DropdownMenuItem(
+                    text = { Text("Outros") },
+                    onClick = { /* Handle send feedback! */ },
+                )
             }
         }
     }
